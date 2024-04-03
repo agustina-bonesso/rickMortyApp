8387314fc65f8ad1350cc75ec76ef9ff2e09acb2
Steffen Zierold <steffen.zierold@outlook.com>
2024-04-03 11:05:19 +0200

    second task fetch data

--- recap-project-3_rick-and-morty-app/index.js
+++ recap-project-3_rick-and-morty-app/index.js
@@ -1,7 +1,5 @@
 const cardContainer = document.querySelector('[data-js="card-container"]');
-const searchBarContainer = document.querySelector(
-  '[data-js="search-bar-container"]'
-);
+const searchBarContainer = document.querySelector('[data-js="search-bar-container"]');
 const searchBar = document.querySelector('[data-js="search-bar"]');
 const navigation = document.querySelector('[data-js="navigation"]');
 const prevButton = document.querySelector('[data-js="button-prev"]');
@@ -12,3 +10,26 @@ const pagination = document.querySelector('[data-js="pagination"]');
 const maxPage = 1;
 const page = 1;
 const searchQuery = "";
+
+// fetch data
+import { CharacterCard } from "./components/CharacterCard/CharacterCard.js";
+cardContainer.innerHTML = "";
+async function fetchCharacters() {
+  const response = await fetch("https://rickandmortyapi.com/api/character");
+  const data = await response.json();
+
+  // console.log(data);
+  return data.results;
+}
+
+fetchCharacters();
+
+async function createSingleCard() {
+  const cardArray = await fetchCharacters();
+  console.log(cardArray);
+  cardArray.forEach((element) => {
+    const card = CharacterCard(element);
+    cardContainer.append(card);
+  });
+}
+createSingleCard();
