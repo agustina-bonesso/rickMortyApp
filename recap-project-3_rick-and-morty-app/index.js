3ce869306bd40a9ac2350822a5c29bc43135b6b7
Maria Agustina Bonesso Immerso <agustina.bonesso@gmail.com>
2024-04-03 12:47:43 +0200

    commit task3 navigation page

--- recap-project-3_rick-and-morty-app/index.js
+++ recap-project-3_rick-and-morty-app/index.js
@@ -1,5 +1,7 @@
 const cardContainer = document.querySelector('[data-js="card-container"]');
-const searchBarContainer = document.querySelector('[data-js="search-bar-container"]');
+const searchBarContainer = document.querySelector(
+  '[data-js="search-bar-container"]'
+);
 const searchBar = document.querySelector('[data-js="search-bar"]');
 const navigation = document.querySelector('[data-js="navigation"]');
 const prevButton = document.querySelector('[data-js="button-prev"]');
@@ -7,29 +9,54 @@ const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
 // States
-const maxPage = 1;
-const page = 1;
+let maxPage;
+let page = 1;
 const searchQuery = "";
+let url = "https://rickandmortyapi.com/api/character";
+let nextUrl;
+let prevUrl;
 
 // fetch data
 import { CharacterCard } from "./components/CharacterCard/CharacterCard.js";
-cardContainer.innerHTML = "";
-async function fetchCharacters() {
-  const response = await fetch("https://rickandmortyapi.com/api/character");
-  const data = await response.json();
 
-  // console.log(data);
+async function fetchCharacters() {
+  cardContainer.innerHTML = "";
+  const response = await fetch(url);
+  const data = await response.json();
+  maxPage = data.info.pages;
+  nextUrl = data.info.next;
+  prevUrl = data.info.prev;
+
   return data.results;
 }
 
-fetchCharacters();
-
 async function createSingleCard() {
   const cardArray = await fetchCharacters();
-  console.log(cardArray);
+
   cardArray.forEach((element) => {
     const card = CharacterCard(element);
     cardContainer.append(card);
   });
 }
 createSingleCard();
+
+nextButton.addEventListener("click", () => {
+  if (page < 42) {
+    page = page + 1;
+    pagination.innerHTML = ` ${page} / ${maxPage}`;
+    url = nextUrl;
+    fetchCharacters();
+    createSingleCard();
+    console.log(nextUrl);
+  }
+});
+
+prevButton.addEventListener("click", () => {
+  if (page > 1) {
+    page = page - 1;
+    pagination.innerHTML = ` ${page} /  ${maxPage}`;
+    url = prevUrl;
+    fetchCharacters();
+    createSingleCard();
+  }
+});
